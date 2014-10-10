b20aa4841c8ec62397c3d21371d71933e67a4002
Amaury-Courjault <amaury.courjault@insa-lyon.fr>
2014-10-10 11:25:05 +0200

    MAJ ihm

--- ihm.pl
+++ ihm.pl
@@ -3,14 +3,17 @@ plateau([[12,0],[2,1],[0,0],[2,4],[1,0],[0,6],[3,0],[0,12]]).
 
 print_ihm([]).
 print_ihm([T|[]]) :- 
-	print_line(T).
+	print_line(T),!.
 print_ihm([T|Q]) :- 
 	print_line(T),
-	print('\n---------------\n'),
+	print('\n-----------------\n'),
 	print_ihm(Q).
 	
 print_line([]).
 print_line([T1,T2|_]) :-
+	S is T1 + T2,
+	print(S),
+	print('  '),
 	print_jeton(T1,'x'),
 	print_jeton(T2,'o').
 	
