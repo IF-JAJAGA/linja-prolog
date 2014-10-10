f5fd4f2eea050330066a36f560a88c27a9ba3a5d
albertorevel <alberto.revel@gmail.com>
2014-10-10 17:47:08 +0200

    gagnant.pl demi arrangé. Inverser ne fonctionne pas.

--- gagnant.pl
+++ gagnant.pl
@@ -1,25 +1,31 @@
 plateau([[12,0],[2,1],[0,0],[2,4],[1,0],[0,6],[3,0],[0,12]]).
+plateau2([[0,12],[0,1],[0,5],[0,0],[1,0],[0,6],[3,0],[0,12]]).
+plateau3([[0,12],[4,1],[0,5],[3,0],[0,0],[7,0],[26,0],[4,0]]).
+plateau4([[0,12],[4,1],[0,5],[3,0],[0,0],[7,0],[26,0],[4,2]]).
+plateau5([[0,12],[4,1],[0,5],[3,0],[0,0],[7,0],[26,6],[4,0]]).
+plateau6([[0,12],[4,1],[0,5],[3,0],[0,0],[7,8],[26,0],[4,0]]).
+plateau7([[0,12],[0,1],[0,5],[0,0],[0,0],[0,0],[0,0],[0,0]]).
 
 comp_fini([]).
-comp_fini(A,B,C,D|L) :-
-	LI = (A,B,C,D)
-	inverser_thermes(L,LA),
-	fini(LI) OR fini(LA).
-	%je sais pas comment écrire le OR.
+comp_fini([A,B,C,D|L]) :-
+	LI = [A,B,C,D],
+%	inverser_thermes(L,LA),
+	fini(0,LI),
+%	fini(0,LA).
 	
-inverser_thermes([],[]).
-inverser_thermes([T|Q],L) :-
-	(reverse(T),R),
-	R2 = (L,R),
-	inverser_thermes(Q,R2).
+	
+%inverser_thermes([],R) .
+%inverser_thermes([T|Q],L) :-
+%	reverse(T,R),
+%	R2 = (L,R),
+%	inverser_thermes(Q,R2).
 	
 	
 
-fini([]).
-fini([T|Q]) :- 
+fini(0,[]).
+fini(0,[T|Q]) :- 
 	T = [TT|TQ],
-	TT == 0,
-	fini (Q).
+	fini(TT,Q).
 
 	
 %fonction "fini" : le jeu est fini ? --> utiliser la fonction reverse deux fois (petite et grande liste)!
