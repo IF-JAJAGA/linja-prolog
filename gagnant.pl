bb2c921563acdc35d8e9b87381bec029de176637
albertorevel <alberto.revel@gmail.com>
2014-10-10 13:37:44 +0200

    Ecriture gagnant.pl, ne fonctionne pas

--- gagnant.pl
+++ gagnant.pl
@@ -1,5 +1,26 @@
 plateau([[12,0],[2,1],[0,0],[2,4],[1,0],[0,6],[3,0],[0,12]]).
 
+comp_fini([]).
+comp_fini(A,B,C,D|L) :-
+	LI = (A,B,C,D)
+	inverser_thermes(L,LA),
+	fini(LI) OR fini(LA).
+	%je sais pas comment écrire le OR.
+	
+inverser_thermes([],[]).
+inverser_thermes([T|Q],L) :-
+	(reverse(T),R),
+	R2 = (L,R),
+	inverser_thermes(Q,R2).
+	
+	
 
+fini([]).
+fini([T|Q]) :- 
+	T = [TT|TQ],
+	TT == 0,
+	fini (Q).
+
+	
 %fonction "fini" : le jeu est fini ? --> utiliser la fonction reverse deux fois (petite et grande liste)!
 %fonction "gagnant" : calculer les points de chaque jou
