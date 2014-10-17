02a87a2f79be8a97ff3871eb5a1eb0f58908512d
Nautigsam <aurelien.bertron@insa-lyon.fr>
2014-10-17 11:56:25 +0200

    ajoute l'analyse séparée du premier coup

--- regles.pl
+++ regles.pl
@@ -1,5 +1,6 @@
 :- dynamic plateau/1.
 plateau([[12,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,12]]).
+:- include('deplacement.pl').
 
 % Prouve que l'element d'indice N a pour valeur Val dans List.
 nth_element(0,Val,[Val|_]).
@@ -19,27 +20,18 @@ calcul_cp(Coups,CP) :-
 	CP is CP1 + X.
 
 % Prouve que tous les coups de la liste sont dans les limites du plateau.
-sur_plateau(_,[]).
-sur_plateau(0,Coups) :-
+sur_plateau([]).
+sur_plateau(Coups) :-
 	Coups = [T|Q],
 	nth_element(0,Depart,T),
 	nth_element(1,PM,T),
+	% PM peut être positif ou négatif selon le joueur
 	Depart >= 0,
 	Depart =< 7,
 	Arrivee is Depart + PM,
 	Arrivee >= 0,
 	Arrivee =< 7,
-	sur_plateau(0,Q),!.
-sur_plateau(1,Coups) :-
-	Coups = [T|Q],
-	nth_element(0,Depart,T),
-	nth_element(1,PM,T),
-	Depart >= 0,
-	Depart =< 7,
-	Arrivee is Depart - PM,
-	Arrivee >= 0,
-	Arrivee =< 7,
-	sur_plateau(1,Q),!.
+	sur_plateau(Q),!.
 
 joueur_existe(0).
 joueur_existe(1).
@@ -56,13 +48,41 @@ pion_joueur(J,Coups) :-
 	Npions > 0,
 	pion_joueur(J,Q),!.
 
+% Prouve que la case Ncase a Npions
+nombre_pions(Ncase,Npions) :-
+	plateau(P),
+	nth_element(Ncase,Case,P),
+	nth_element(0,P0,Case),
+	nth_element(1,P1,Case),
+	Npions = P0 + P1.
+
+% Prouve que la case est pleine, toujours faux si l'on est dans les extrémités du plateau
+case_pleine(Ncase) :-
+	Ncase > 0,
+	Ncase < 7,
+	nombre_pions(Ncase,Npions),
+	Npions >= 6.
+
 % Prouve que les coups sont possibles pour le joueur J en CP mouvements.
+est_licite([], _, 0).
 est_licite(Coups,J,CP) :-
 	Coups = [T|Q],
 	T = [TCoup|QCoup],
-	sur_plateau(J,Coups),
+	sur_plateau(Coups),
 	pion_joueur(J,Coups),
 	calcul_cp(Coups,CP).
+
+est_licite(Coups,J) :-
+	Coups = [T|Q],
+	plateau(P),
+	nth_element(0,Depart,T),
+	nth_element(1,PM,T),
+	Arrivee is Depart + PM,
+	not(case_pleine(Arrivee)),
+	nombre_pions(Arrivee,CP),
+	increment(Arrivee,J,P),
+	est_licite(Q, J, CP),
+	decrement(Arrivee,J,P).
 
 % ======================================== TESTS ==========================================
 :- begin_tests(regles).
@@ -75,17 +95,27 @@ test(calcul_cp_3) :-
 	calcul_cp([[2,2]],2).
 
 test(sur_plateau_1) :-
-	not(sur_plateau(0,[[-1,1]])).
+	not(sur_plateau([[-1,1]])).
 test(sur_plateau_2) :-
-	not(sur_plateau(0,[[8,1]])).
+	not(sur_plateau([[8,1]])).
 test(sur_plateau_3) :-
-	not(sur_plateau(0,[[7,1]])).
+	not(sur_plateau([[7,1]])).
 test(sur_plateau_4) :-
-	not(sur_plateau(1,[[0,1]])).
+	not(sur_plateau([[0,-1]])).
 test(sur_plateau_5) :-
-	sur_plateau(1,[[7,1]]).
+	sur_plateau([[7,-1]]).
 test(sur_plateau_6) :-
-	sur_plateau(0,[[0,1]]).
+	sur_plateau([[0,1]]).
+
+test(case_pleine_1) :-
+	not(case_pleine(0)).
+test(case_pleine_2) :-
+	not(case_pleine(7)).
+test(case_pleine_3, [
+											setup(switch_plateaux(Pold,[[3,0],[2,0],[6,0],[1,0],[0,1],[0,6],[0,3],[0,2]])),
+											cleanup(switch_plateaux([[3,0],[2,0],[6,0],[1,0],[0,1],[0,6],[0,3],[0,2]],Pold))
+										]) :-
+	case_pleine(2).
 
 switch_plateaux(Pold,Pnew) :-
 	plateau(Pold),
