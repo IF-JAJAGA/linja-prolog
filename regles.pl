17853d5956186911606916ac0985d693f5b476c3
Nautigsam <aurelien.bertron@insa-lyon.fr>
2014-10-17 11:03:33 +0200

    test si le coup ne mène pas en dehors du plateau

--- regles.pl
+++ regles.pl
@@ -19,13 +19,27 @@ calcul_cp(Coups,CP) :-
 	CP is CP1 + X.
 
 % Prouve que tous les coups de la liste sont dans les limites du plateau.
-sur_plateau([]).
-sur_plateau(Coups) :-
+sur_plateau(_,[]).
+sur_plateau(0,Coups) :-
 	Coups = [T|Q],
-	nth_element(0,X,T),
-	X >= 0,
-	X =< 7,
-	sur_plateau(Q).
+	nth_element(0,Depart,T),
+	nth_element(1,PM,T),
+	Depart >= 0,
+	Depart =< 7,
+	Arrivee is Depart + PM,
+	Arrivee >= 0,
+	Arrivee =< 7,
+	sur_plateau(0,Q),!.
+sur_plateau(1,Coups) :-
+	Coups = [T|Q],
+	nth_element(0,Depart,T),
+	nth_element(1,PM,T),
+	Depart >= 0,
+	Depart =< 7,
+	Arrivee is Depart - PM,
+	Arrivee >= 0,
+	Arrivee =< 7,
+	sur_plateau(1,Q),!.
 
 joueur_existe(0).
 joueur_existe(1).
@@ -40,27 +54,38 @@ pion_joueur(J,Coups) :-
 	nth_element(Depart,Case,P),
 	nth_element(J,Npions,Case),
 	Npions > 0,
-	pion_joueur(J,Q).
+	pion_joueur(J,Q),!.
 
 % Prouve que les coups sont possibles pour le joueur J en CP mouvements.
 est_licite(Coups,J,CP) :-
 	Coups = [T|Q],
 	T = [TCoup|QCoup],
-	sur_plateau(Coups),
+	sur_plateau(J,Coups),
 	pion_joueur(J,Coups),
 	calcul_cp(Coups,CP).
 
 % ======================================== TESTS ==========================================
 :- begin_tests(regles).
 
-test(calcul_cp) :-
-	calcul_cp([],0),
-	calcul_cp([[1,1],[1,2]],3),
+test(calcul_cp_1) :-
+	calcul_cp([],0).
+test(calcul_cp_2) :-
+	calcul_cp([[1,1],[1,2]],3).
+test(calcul_cp_3) :-
 	calcul_cp([[2,2]],2).
 
-test(sur_plateau) :-
-	not(sur_plateau([-1,1])),
-	not(sur_plateau([8,1])).
+test(sur_plateau_1) :-
+	not(sur_plateau(0,[[-1,1]])).
+test(sur_plateau_2) :-
+	not(sur_plateau(0,[[8,1]])).
+test(sur_plateau_3) :-
+	not(sur_plateau(0,[[7,1]])).
+test(sur_plateau_4) :-
+	not(sur_plateau(1,[[0,1]])).
+test(sur_plateau_5) :-
+	sur_plateau(1,[[7,1]]).
+test(sur_plateau_6) :-
+	sur_plateau(0,[[0,1]]).
 
 switch_plateaux(Pold,Pnew) :-
 	plateau(Pold),
@@ -71,6 +96,7 @@ test(pion_joueur, [
 										setup(switch_plateaux(Pold,[[12,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,12]])),
 										cleanup(switch_plateaux([[12,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,12]],Pold))
 									]) :-
+	pion_joueur(0,[[0,1]]),
 	pion_joueur(0,[[0,1],[0,2]]).
 
 test(est_licite) :-
