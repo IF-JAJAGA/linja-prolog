573b26f8446263e0bf4b99c5e4fa0bee84fb58c7
unknown <jmarchal@IF501-204-12.insa-lyon.fr>
2014-10-17 10:22:03 +0200

    Correction de sur_plateau et pion_joueur

--- regles.pl
+++ regles.pl
@@ -4,9 +4,9 @@ plateau([[12,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,12]]).
 % Prouve que l'element d'indice N a pour valeur Val dans List.
 nth_element(0,Val,[Val|_]).
 nth_element(N,Val,List) :-
-	List = [_|Tail],
-	nth_element(N1,Val,Tail),
-	N is N1+1.
+	List = [_|Q],
+	N1 is N-1,
+	nth_element(N1,Val,Q).
 
 % Prouve que la liste de coups consomme CP points de mouvement.
 calcul_cp(Coups,CP) :-
@@ -21,10 +21,12 @@ calcul_cp(Coups,CP) :-
 % Prouve que tous les coups de la liste sont dans les limites du plateau.
 sur_plateau([]).
 sur_plateau(Coups) :-
-	Coups = [T|_],
+	Coups = [T|Q],
 	nth_element(0,X,T),
+	!,
 	X >= 0,
-	X =< 7.
+	X =< 7,
+	sur_plateau(Q),!.
 
 joueur_existe(0).
 joueur_existe(1).
@@ -33,12 +35,14 @@ joueur_existe(1).
 pion_joueur(_,[]).
 pion_joueur(J,Coups) :-
 	joueur_existe(J),
-	plateau(P),
 	Coups = [T|Q],
+	plateau(P),
 	nth_element(0,Depart,T),
 	nth_element(Depart,Case,P),
 	nth_element(J,Npions,Case),
-	Npions > 0.
+	!,
+	Npions > 0,
+	pion_joueur(J,Q),!.
 
 % Prouve que les coups sont possibles pour le joueur J en CP mouvements.
 est_licite(Coups,J,CP) :-
