da3f5f104bdf412e9a58ff128654a6d7da59446f
Amaury Courjault <amaury.courjault@insa-lyon.fr>
2014-10-22 22:13:48 +0200

    Add boucle d'affichage ihm

--- ihm.pl
+++ ihm.pl
@@ -32,8 +32,38 @@ print_jeton(N,M) :-
 %send(button(hello, message(@prolog, format, 'Hi There˜n')), open).structure of a prolog
 initialise :-
 	%initialise_database,
-	create_GUI_components.
+	create_GUI_components,
+	plateau(X),
+	print_GUI_Jetons(X).
 	%free_GUI_components.
+	
+	
+init:-
+	create_plateau,
+	plateau(X),
+	print_GUI_Jetons(X,1).
+
+print_GUI_Jetons([]).
+print_GUI_Jetons([T|Q],NumLine):-
+	print_GUI_line(T,NumLine),
+	Num2 is NumLine + 1,
+	print_GUI_Jetons(Q,Num2).
+	
+print_GUI_line([],_).
+print_GUI_line([T1,T2|_],NumLine) :-
+	print_GUI_pion(T1,0,NumLine,colour(red)),
+	print_GUI_pion(T2,T1,NumLine,colour(black)).
+	
+print_GUI_pion(0,_,_,_).
+print_GUI_pion(N,M,NumLine,Color):-
+	N \==0,
+	%Ajoute le cercle à l'image
+	new(C,circle(25)),
+	send(C,fill_pattern, Color),
+	send(@p, display, C, point(12.5+(50*(NumLine-1)),25+(N*30)+(M*30))),
+	N1 is N -1,
+	print_GUI_pion(N1,M,NumLine,Color).
+	
 
 create_GUI_components :-
 	new(@p, picture('Test 1')),
@@ -48,10 +78,22 @@ create_GUI_components :-
 	send(@p, display, new(@l6, line(300,0,300,750,none))),
 	send(@p, display, new(@l7, line(350,0,350,750,none))),
 	send(@p, display, new(button(nextStep, message(@p,return,@nil))), point(550,25)),
-	send(@r, fill_pattern, colour(red)),
+	%send(@r, fill_pattern, colour(red)),
 	send(@b, fill_pattern, colour(black)),
 	get(@p, confirm, @nil),
 	move.
+	
+create_plateau :-
+	new(@p, picture('Test 1')),
+	send(@p,open),
+	send(@p, display, new(@l1, line(50,0,50,750,none))),
+	send(@p, display, new(@l2, line(100,0,100,750,none))),
+	send(@p, display, new(@l3, line(150,0,150,750,none))),
+	send(@p, display, new(@l4, line(200,0,200,750,none))),
+	send(@p, display, new(@l5, line(250,0,250,750,none))),
+	send(@p, display, new(@l6, line(300,0,300,750,none))),
+	send(@p, display, new(@l7, line(350,0,350,750,none))),
+	send(@p, display, new(button(nextStep, message(@p,return,@nil))), point(550,25)).
 	
 move :-
 	free(@b),
@@ -86,41 +128,16 @@ ask_name(Name) :-
 	free(D),
 	Rval \== @nil,
 	Name = Rval.
-	
-:- use_module(library(pce)).
-
-:- pce_begin_class(name_asker, dialog,
-                   "Modal prompter for a name").
-
-initialise(W, Label:[name]) :->
-        "Initialise the window and fill it"::
-        send_super(W, initialise(Label)),
-        send(W, append(text_item(name))),
-        send(W, append(button(ok))),
-        send(W, append(button(cancel))),
-        send(W, default_button(ok)).
-        
-ok(W) :->
-        "User pressed the OK button"::
-        get(W, member(name), NameItem),
-        get(NameItem, selection, Typed),
-        send(W, return, Typed).
-
-cancel(W) :->
-        "User pressed the Cancel button"::
-        send(W, return(@nil)).
-
-prompt(W, Value:name) :<-
-        "Open it, destroy it and return the result"::
-        get(W, confirm, Value),
-        free(W).
-
-:- pce_end_class.
 
 test(42).
 
 test_repeat :-
 	test(X),
+	print(X),
+	X2 is X-1,
+	X is X2,
 	repeat,
-	X is X-1,
-	X > 0.
+	nl,
+	print(X2),
+	read(A),
+	(X =:=1).
